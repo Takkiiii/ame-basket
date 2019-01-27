fd7d713776f2b6e44f4f68030911bfa8fe281d8b
ShoTAKIZAWA <takizawa.sho@imagicalab.co.jp>
2019-01-27 14:31:28 +0900

    update script;

--- dist/jsx/hostscript.jsx
+++ dist/jsx/hostscript.jsx
@@ -44,19 +44,25 @@ function encodeVideoClips(json) {
         app.enableQE();
         const encoder = app.encoder;
         const clips = _getClips();
-        for (var i = 0; i < indexes.length; i++) {
-            var index = indexes[i];
-            var clip = clips[index];
-            if (clip === null) {
-                alert('clip sis null');
-                continue;
-            }
-            var fullOutputPath = new File(exportPath.fsName + getSep() + clip.name).fsName;
+        if (clips) {
+            app.encoder.launchEncoder();
             const workArea = app.encoder.ENCODE_WORKAREA;
             const boolRemoveUponCompletion = 1;
-            encoder.encodeProjectItem(clip.projectItem, fullOutputPath, presetPath, workArea, boolRemoveUponCompletion, clip.start, clip.end);
+            for (var i = 0; i < indexes.length; i++) {
+                var index = indexes[i];
+                var clip = clips[index];
+                if (clip === null) {
+                    alert('clip sis null');
+                    continue;
+                }
+                var fullOutputPath = new File(exportPath.fsName + getSep() + clip.name).fsName;
+                alert(clip.projectItem, fullOutputPath, presetPath, workArea, boolRemoveUponCompletion, clip.start, clip.end);
+                encoder.encodeProjectItem(clip.projectItem, fullOutputPath, presetPath, workArea, boolRemoveUponCompletion, clip.start, clip.end);
+            }
+            encoder.startBatch();
+        } else {
+            alert('No clips not found');
         }
-        encoder.startBatch();
     } catch(e) {
         alert(e);
         throw e;
@@ -92,4 +98,46 @@ function _getClips() {
 function getClips() {
     var result = _getClips();
     return JSON.stringify(result);
+}
+
+function encode(json) {
+    // app.encoder.bind('onEncoderJobComplete',	$._PPP_.onEncoderJobComplete);		
+	// app.encoder.bind('onEncoderJobError', 		$._PPP_.onEncoderJobError);
+	// app.encoder.bind('onEncoderJobProgress', 	$._PPP_.onEncoderJobProgress);
+	// app.encoder.bind('onEncoderJobQueued', 		$._PPP_.onEncoderJobQueued);
+    // app.encoder.bind('onEncoderJobCanceled',	$._PPP_.onEncoderJobCanceled);
+    var outputPresetPath =new File(json.presetPath).fsName;
+	var projRoot = app.project.rootItem.children;
+
+	if (projRoot.numItems){
+        var children = app.project.rootItem.children;
+        alert(children.length);
+        if (children.length > 0) {
+            app.encoder.launchEncoder();	// This can take a while; let's get the ball rolling.
+            for (var i = 0; i < children.length; i++) {
+                var firstProjectItem = children[i];
+                if (firstProjectItem){
+                    var fileOutputPath	= Folder.selectDialog("Choose the output directory");
+                    if (fileOutputPath){
+                        var outputName	= firstProjectItem.name.search('[.]');
+                        if (outputName == -1) {
+                            outputName	= firstProjectItem.name.length;
+                        }
+                        outFileName	= firstProjectItem.name.substr(0, outputName);
+                        outFileName	= outFileName.replace('/', '-');
+                        var completeOutputPath	= fileOutputPath.fsName + getSep() + outFileName;
+                        var removeFromQueue	= true;
+                        var rangeToEncode = app.encoder.ENCODE_IN_TO_OUT;
+                        app.encoder.encodeProjectItem(firstProjectItem, completeOutputPath, outputPresetPath, rangeToEncode, removeFromQueue); 
+                        app.encoder.startBatch();
+                    }
+                }
+            }
+        } else {
+            alert("No project item found.");
+            // $._PPP_.updateEventPanel("No project items found.");
+        }
+	} else {
+        alert("Project is empty.");
+	}
 }
