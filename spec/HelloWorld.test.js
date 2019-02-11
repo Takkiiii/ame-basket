330e29a6f6092c4f4e123ee88924bb23bb516146
Takkiiii <takizawa@photron.co.jp>
2019-02-11 19:51:31 +0900

    done vue.test :)

--- spec/HelloWorld.test.js
+++ spec/HelloWorld.test.js
@@ -1,4 +1,22 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import HelloWorld from 'components/HelloWorld';
 
-test('it says hello', () => {
-    expect(1 + 1).toBe(2)
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const factory = () => {
+  const state = { appName: 'Adobe Premiere Pro CC', appVersion: "12.0" };
+  const store = new Vuex.Store({state}); 
+  return shallowMount(HelloWorld, {
+    localVue: localVue,
+    store: store
   })
+};
+
+describe('Component', () => {
+  test('is a Vue instance', () => {
+    const wrapper = factory();
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
